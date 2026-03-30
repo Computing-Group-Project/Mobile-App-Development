39304757d7c078fd5215ad4d87b29b044216dc3a
MSP <166158378+MaleeshaSP@users.noreply.github.com>
2026-03-30 16:44:00 +0530

    add group provider for state management
    - implemented GroupProvider using ChangeNotifier
    - integrated Firebase Authentication to get current user ID
    - added stream for real-time group updates
    - added stream for group expenses
    - implemented createGroup function
    - implemented addExpense function
    - connected provider with service layer for data handling

---
+++ lib/features/groups/providers/group_provider.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '../models/group_model.dart';
+import '../models/expense_model.dart';
+import '../services/group_service.dart';
+
+class GroupProvider extends ChangeNotifier {
+  final GroupService _service = GroupService();
+
+  String get uid {
+    final u = FirebaseAuth.instance.currentUser;
+    if (u == null) throw Exception("No user");
+    return u.uid;
+  }
+
+  Stream<List<GroupModel>> get groupsStream =>
+      _service.streamUserGroups(uid);
+
+  Stream<List<SharedExpense>> expensesStream(String groupId) =>
+      _service.streamGroupExpenses(groupId);
+
+  Future<void> createGroup(String name, List<GroupMember> members) async {
+    await _service.createGroup(
+      name: name,
+      createdBy: uid,
+      members: members,
+    );
+  }
+
+  Future<void> addExpense(SharedExpense e) async {
+    await _service.addExpense(e);
+  }
+}
